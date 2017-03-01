8b0967a1c4922f698a5bb7f2713a7f063767b568
Jesse Smith <jesse.etan.smith@gmail.com>
2017-02-28 19:34:15 -0500

    added english language to the cards

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -4,6 +4,7 @@ $.ajax({
   complete: function(data) {
     var chengyu = data.responseJSON[0];
     $("#daily").html("<div id=\"card_chinese\">"+chengyu.chinese+"<div><br />"
-                   + "<div id=\"card_sentence\">"+chengyu.sentence_list[0]+"</div>");
+                   + "<div id=\"card_english\"><span id=\"type\">意思:</span> "+chengyu.english_list[0]+"<div><br />"
+                   + "<div id=\"card_sentence\"><span id=\"type\">例句:</span> "+chengyu.sentence_list[0]+"</div>");
    }
 });
